97e3ab5125e9166e78ddd5b531a326b6316171dc
MFHava <mfh@live.at>
2018-04-05 01:23:44 +0200

    new approach to constructing error_codes

--- inc/cwc/internal/error_code.hpp
+++ inc/cwc/internal/error_code.hpp
@@ -12,34 +12,39 @@
 
 namespace cwc {
 	namespace internal {
+		template<uint8 Level0 = 0, uint8 Level1 = 0, uint8 Level2 = 0, uint8 Level3 = 0>
+		struct error_code_helper final : std::integral_constant<uint32, (1 << 31) | (Level0 << 23) | (Level1 << 15) | (Level2 << 7) | Level3> {
+			static_assert(Level3 < 1 << 8, "last inheritance level only has 7bits");
+		};
+
 		//exceptions of the standard library: http://en.cppreference.com/w/cpp/error/exception
 		enum class error_code : uint32 {
 			no_error,
-			std98_exception                    = static_cast<uint32>(1) << 31,
-				std98_logic_error              = +std98_exception | static_cast<uint32>(1) << 23,
-					std98_invalid_argument     = 	+std98_logic_error | static_cast<uint32>(1) << 15,
-					std98_domain_error         = 	+std98_logic_error | static_cast<uint32>(2) << 15,
-					std98_length_error         = 	+std98_logic_error | static_cast<uint32>(3) << 15,
-					std98_out_of_range         = 	+std98_logic_error | static_cast<uint32>(4) << 15,
-					std11_future_error         = 	+std98_logic_error | static_cast<uint32>(5) << 15,
-				std98_runtime_error            = +std98_exception | static_cast<uint32>(2) << 23,
-					std98_range_error          = 	+std98_runtime_error | static_cast<uint32>(1) << 15,
-					std98_overflow_error       = 	+std98_runtime_error | static_cast<uint32>(2) << 15,
-					std98_underflow_error      = 	+std98_runtime_error | static_cast<uint32>(3) << 15,
-					std11_regex_error          = 	+std98_runtime_error | static_cast<uint32>(4) << 15,
-					std11_system_error         = 	+std98_runtime_error | static_cast<uint32>(5) << 15,
-						std11_ios_base_failure = 		+std11_system_error | static_cast<uint32>(1) << 7,
-						std17_filesystem_error = 		+std11_system_error | static_cast<uint32>(2) << 7,
-				std98_bad_typeid               = +std98_exception | static_cast<uint32>(3) << 23,
-				std98_bad_cast                 = +std98_exception | static_cast<uint32>(4) << 23,
-					std17_bad_any_cast         = 	+std98_bad_cast | static_cast<uint32>(1) << 15,
-				std98_bad_alloc                = +std98_exception | static_cast<uint32>(5) << 23,
-					std11_bad_array_new_length = 	+std98_bad_alloc | 1 << 15,
-				std98_bad_exception            = +std98_exception | static_cast<uint32>(6) << 23,
-				std11_bad_weak_ptr             = +std98_exception | static_cast<uint32>(7) << 23,
-				std11_bad_function_call        = +std98_exception | static_cast<uint32>(8) << 23,
-				std17_bad_variant_access       = +std98_exception | static_cast<uint32>(9) << 23,
-				std17_bad_optional_access      = +std98_exception | static_cast<uint32>(10) << 23,
+			std98_exception = error_code_helper<>::value,
+				std98_logic_error = error_code_helper<1>::value,
+					std98_invalid_argument = error_code_helper<1, 1>::value,
+					std98_domain_error = error_code_helper<1, 2>::value,
+					std98_length_error = error_code_helper<1, 3>::value,
+					std98_out_of_range = error_code_helper<1, 4>::value,
+					std11_future_error = error_code_helper<1, 5>::value,
+				std98_runtime_error = error_code_helper<2>::value,
+					std98_range_error = error_code_helper<2, 1>::value,
+					std98_overflow_error = error_code_helper<2, 2>::value,
+					std98_underflow_error = error_code_helper<2, 3>::value,
+					std11_regex_error = error_code_helper<2, 4>::value,
+					std11_system_error = error_code_helper<2, 5>::value,
+						std11_ios_base_failure = error_code_helper<2, 5, 1>::value,
+						std17_filesystem_error = error_code_helper<2, 5, 2>::value,
+				std98_bad_typeid = error_code_helper<3>::value,
+				std98_bad_cast = error_code_helper<4>::value,
+					std17_bad_any_cast = error_code_helper<4, 1>::value,
+				std98_bad_alloc = error_code_helper<5>::value,
+					std11_bad_array_new_length = error_code_helper<5, 1>::value,
+				std98_bad_exception = error_code_helper<6>::value,
+				std11_bad_weak_ptr = error_code_helper<7>::value,
+				std11_bad_function_call = error_code_helper<8>::value,
+				std17_bad_variant_access = error_code_helper<9>::value,
+				std17_bad_optional_access = error_code_helper<10>::value,
 		};
 
 		auto store_exception(std::exception_ptr eptr) noexcept -> error_code;

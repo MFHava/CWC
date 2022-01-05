e02365323a4db7c3271fca1c09c5e24840704e19
MFHava <mfh@live.at>
2022-01-05 19:51:51 +0100

    added stubs for unit tests

---
+++ test/cwc/main.cpp
@@ -0,0 +1,10 @@
+
+//          Copyright Michael Florian Hava.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CWC_TEST_DLL
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#endif
